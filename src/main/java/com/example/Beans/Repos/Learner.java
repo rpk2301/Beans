b24c65392a6ac7db550d69190da2151112bf875d
Ricky Kyte <137026079+rpk2301@users.noreply.github.com>
2023-08-18 14:27:45 -0400

    In Progress

---
+++ src/main/java/com/example/Beans/Repos/Learner.java
@@ -0,0 +1,8 @@
+package com.example.Beans.Repos;
+
+public interface Learner {
+
+    public void learn(double numberOfHours);
+
+
+}
---
+++ src/main/java/com/example/Beans/SpringBeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.Beans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansApplication.class, args);
+	}
+
+}
